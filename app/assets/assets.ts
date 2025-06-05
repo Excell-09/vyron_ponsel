4ad39db75b3c5adeda24746fb6a86cfb5cc07d65
Excell-09 <excell.agcb12@gmail.com>
2025-06-05 12:16:50 +0700

    add shacn

---
+++ app/assets/assets.ts
@@ -0,0 +1,5 @@
+import logo from "./logo.png";
+import horizontalLOgo from "./horizontal_logo.png";
+import transparentHorizontalLogo from "./transparent_logo.png";
+
+export { logo, horizontalLOgo, transparentHorizontalLogo };
